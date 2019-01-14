4d9764c9d3bfe0b1da95312998367c3e747ce52e
jianghongbin <JIANGHONGBIN@kingsoft.com>
2019-01-14 16:52:54 +0800

    [feat]-测试接口

--- taobao-counpon/src/main/java/taobao/counpon/controller/HBaseController.java
+++ taobao-counpon/src/main/java/taobao/counpon/controller/HBaseController.java
@@ -1,5 +1,6 @@
 package taobao.counpon.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import taobao.core.model.APIResponse;
 import org.slf4j.Logger;
@@ -25,7 +26,15 @@ public class HBaseController {
         logger.info("HModel -> {}", usersCounpon);
         Boolean value =  hbaseRepository.upsert(usersCounpon);
         logger.info("value -> {}", value);
-        return ResponseEntity.ok(new APIResponse<Boolean>(0, value));
+        return ResponseEntity.ok(new APIResponse<>(0, value));
     }
+
+    @RequestMapping(value = "/usercounpons/{rowkey}")
+    ResponseEntity<?> testHbaseGet (@PathVariable("rowkey") String rowkey) {
+        UsersCounpon usersCounpon = hbaseRepository.findOne(rowkey);
+        logger.info("usersCounpon -> {}", usersCounpon);
+        return ResponseEntity.ok(new APIResponse<>(0, usersCounpon));
+    }
+
 
 }
