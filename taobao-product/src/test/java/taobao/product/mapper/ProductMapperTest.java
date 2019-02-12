ecf52833de2d91ebaf5d1c3ff8cc952eb53fe1df
jianghongbin <JIANGHONGBIN@kingsoft.com>
2019-02-12 17:52:28 +0800

    [feat]-debug

--- taobao-product/src/test/java/taobao/product/mapper/ProductMapperTest.java
+++ taobao-product/src/test/java/taobao/product/mapper/ProductMapperTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 import taobao.product.App;
 import taobao.product.models.Product;
 import taobao.product.models.ProductSpecsAttributeKey;
@@ -23,9 +24,10 @@ public class ProductMapperTest {
     ProductSpecsAttributeKeyMapper productSpecsAttributeKeyMapper;
 
     @Test
+    @Transactional
     public void testInsert () {
         Product product = new Product();
-        String productId = new DefaultKeyGenerator().generateKey().toString();
+        Long productId = new DefaultKeyGenerator().generateKey().longValue();
         product.setProductId(productId);
         product.setName("macbook pro");
         product.setTitle("京东精选 Apple MacBook Pro 13.3英寸笔记本电脑 深空灰色 2018新款（四核八代i5 8G 256G固态硬盘 MR9Q2CH/A）");
@@ -36,6 +38,7 @@ public class ProductMapperTest {
         productSpecsAttributeKey.setProductId(product.getProductId());
         productSpecsAttributeKey.setCreateTime(new Date());
         productSpecsAttributeKeyMapper.insert(productSpecsAttributeKey);
+        int a = 0/0;
     }
 
 }
